9efd4588f3e17bd2926b090a5cc5cea68abef530
kaedeek <yukise30@icloud.com>
2025-12-22 04:22:56 +0900

    Add: New Features and option | Change README.md

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
 # gzy
 
-**gzy** is a simple CLI tool that makes common Git operations faster and easier.
-You can run commands like `init` / `add` / `commit` / `branch` / `push` / `remote` / `clone` / `checkout` / `status` / `log`  with shorter syntax.
+**gzy** is a simple CLI tool that makes common Git operations faster and easier.  
+It provides shorter and more convenient commands for everyday Git workflows.
+
+Supported commands include:  
+`init`, `add`, `commit`, `branch`, `push`, `remote`, `clone`, `checkout`, `status`, `log`
 
 [![Crates.io](https://img.shields.io/crates/v/gzy.svg)](https://crates.io/crates/gzy)
 [![Crates.io Downloads](https://img.shields.io/crates/d/gzy.svg)](https://crates.io/crates/gzy)
 [![Docs.rs](https://docs.rs/gzy/badge.svg)](https://docs.rs/gzy)
+
+---
+
+## 🚀 Installation
+
+```bash
+cargo install gzy
+```
 
 ---
 
@@ -31,13 +42,20 @@ gzy branch dev
 gzy remote https://github.com/user/repo.git
 
 # Push
+gzy push
 gzy push main
+gzy push main -u
+gzy push main --force
 
 # Clone
 gzy clone https://github.com/user/repo.git
 
-# create new branch
-gzy checkout develop
+# Switch to existing branch
+gzy checkout main
+
+# Create and switch to a new branch
+gzy checkout dev -b
+
 
 # Status
 gzy status
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@ use clap::{Parser, Subcommand};
 use std::process::Command;
 
 #[derive(Parser)]
-#[command(name = "gzy", about = "Make Git commits faster and easier")]
+#[command(
+    name = "gzy",
+    about = "Make Git commits faster and easier"
+)]
 struct CLI {
     #[command(subcommand)]
     commands: Cmd,
@@ -10,59 +13,134 @@ struct CLI {
 
 #[derive(Subcommand)]
 enum Cmd {
-    Add { file: String },
-    Commit { msg: String },
-    Branch { branch_name: String },
-    Remote { url: String },
-    Push { branch: String },
-    Clone { clone_url: String},
-    Checkout { branch: String },
-    
+    Add {
+        file: String,
+    },
+
+    Commit {
+        msg: String,
+    },
+
+    Branch {
+        branch_name: String,
+    },
+
+    Remote {
+        url: String,
+    },
+
+    Push {
+        branch: Option<String>,
+
+        #[arg(short = 'u', long = "set-upstream")]
+        upstream: bool,
+
+        #[arg(short = 'f', long = "force")]
+        force: bool,
+    },
+
+    Clone {
+        clone_url: String,
+    },
+
+    Checkout {
+        branch: String,
+
+        #[arg(short = 'b', long = "create")]
+        create: bool,
+    },
+
     Init,
     Status,
     Log,
-
 }
 
 fn main() {
-    let cli: CLI = CLI::parse();
+    let cli = CLI::parse();
 
     match &cli.commands {
         Cmd::Add { file } => {
-            git(&["add", file]);
+            git(vec!["add".into(), file.clone()]);
         }
 
         Cmd::Commit { msg } => {
-            git(&["commit", "-m", msg]);
+            git(vec!["commit".into(), "-m".into(), msg.clone()]);
         }
 
         Cmd::Branch { branch_name } => {
-            git(&["branch", "-M", branch_name]);
+            git(vec![
+                "branch".into(),
+                "-M".into(),
+                branch_name.clone(),
+            ]);
         }
 
         Cmd::Remote { url } => {
-            git(&["remote", "add", "origin", url]);
+            git(vec![
+                "remote".into(),
+                "add".into(),
+                "origin".into(),
+                url.clone(),
+            ]);
         }
 
-        Cmd::Push { branch } => {
-            git(&["push", "-u", "origin", branch]);
+        Cmd::Push {
+            branch,
+            upstream,
+            force,
+        } => {
+            let mut args = vec!["push".to_string()];
+
+            if *upstream {
+                args.push("-u".into());
+                args.push("origin".into());
+            }
+
+            if let Some(branch) = branch {
+                args.push(branch.clone());
+            }
+
+            if *force {
+                args.push("--force".into());
+            }
+
+            git(args);
         }
 
         Cmd::Clone { clone_url } => {
-            git(&["clone", clone_url]);
+            git(vec!["clone".into(), clone_url.clone()]);
         }
 
-        Cmd::Checkout { branch } => {
-            git(&["checkout", "-b", branch]);
+        Cmd::Checkout { branch, create } => {
+            if *create {
+                git(vec![
+                    "checkout".into(),
+                    "-b".into(),
+                    branch.clone(),
+                ]);
+            } else {
+                git(vec![
+                    "checkout".into(),
+                    branch.clone(),
+                ]);
+            }
         }
 
-        Cmd::Init => git(&["init"]),
-        Cmd::Status => git(&["status"]),
-        Cmd::Log => git(&["log"]),
+        Cmd::Init => {
+            git(vec!["init".into()]);
+        }
+
+        Cmd::Status => {
+            git(vec!["status".into()]);
+        }
+
+        Cmd::Log => {
+            git(vec!["log".into()]);
+        }
     }
 }
 
-fn git(args: &[&str]) {
+fn git(args: Vec<String>) {
     Command::new("git")
         .args(args)
         .status()
